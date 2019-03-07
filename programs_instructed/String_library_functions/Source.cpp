b9b8f48e00cd571d7e346e08e69165d3650ce679
ram-ganesh-16 <ganeshganni111@gmail.com>
2019-03-07 11:24:58 +0530

    changes mades after dynamic allocation

--- programs_instructed/String_library_functions/Source.cpp
+++ programs_instructed/String_library_functions/Source.cpp
@@ -1,92 +1,119 @@
+#include<string.h>
 #include<stdio.h>
-
-void strcpy(char *str2, char *str1)
+#include<stdlib.h>
+#define MAX 100
+//String reverse program
+void str_reverse(char *input, char *output)
 {
-	while (*str1)
+	int length = strlen(input);
+	int i = 0, j = 0;
+	for (i = (length - 1); i >= 0; i--)
 	{
-		*str2 = *str1;
-		*str2++;
-		*str1++;
+		*(output + j) = *(input + i);
+		j++;
 	}
-	*str2 = '\0';
+	*(output + j) = '\0';
 }
-void strcat(char *str1, char *str2)
+//String copy 
+void str_copy(char *input, char *output)
 {
-	while (*str1)
+	int i = 0, j = 0;
+	while (*(input + i) != '\0')
 	{
-		*str1++;
+		*(output + j) = *(input + i);
+		i++;
+		j++;
 	}
-	while (*str2)
-	{
-		*str1 = *str2;
-		*str2++;
-		*str1++;
-	}
-	*str1 = '\0';
-	*str2 = '\0';
+	*(output + j) = '\0';
 }
-int strcmp(char *str1, char *str2)
+//String concatenation
+void str_cat(char *input1, char *input2)
 {
-	char *a, *b;
-	a = str1, b = str2;
-	while (*str1++ == *str2++)
+	int length = strlen(input2);
+	int l = strlen(input1);
+	int i = 0;
+	while (*(input2 + i) != '\0')
 	{
-		if ((*str1) == '\0')
-			return 0;
+		*(input1 + l) = *(input2 + i);
+		l++;
+		i++;
 	}
-	if ((*a - *b) > 0)
-		return 1;
+	*(input1 + l) = '\0';
+}
+//string compares
+void str_compare(char *input1, char *input2)
+{
+	int l1 = strlen(input1);
+	int l2 = strlen(input2);
+	int i = 0;
+	if (l1 > l2)
+		printf("NOT EQUAL");
+	else if (l1 < l2)
+		printf("NOT EQUAL");
 	else
-		return -1;
-}
-void strrev(char *str)
-{
-	char *p, *q, ch;
-	p = str; q = str;
-	int length, i;
-	for (length = 0; str[length] != '\0'; ++length);
-
-	for (i = 0; i < length - 1; i++)
-		q++;
-	for (i = 0; i<length / 2; i++)
 	{
-		ch = *p;
-		*p = *q;
-		*q = ch;
-		p++;
-		q--;
+		while (*(input1 + i) != '\0')
+		{
+			if (*(input1 + i) == *(input2 + i))
+				i++;
+			else
+			{
+				printf("NOT EQUAL");
+				return;
+			}
+		}
+		printf("%s and %s :EQUAL", input1, input2);
 	}
 }
 int main()
 {
-	char string1[50], string2[50];
-	int value, result;
-	printf("Enter the string i.e to been used for further expressions:\n");
-	gets_s(string1);
-	printf("Enter your choice\n1.strrev\n2.strcpy\n3.strcat\n4.strcmp\n");
-	scanf_s("%d", &value);
-	switch (value)
+	char *string1 = (char*)malloc(MAX);
+	char *string2 = (char*)malloc(MAX);
+	int choice = 0;
+	while (1)
 	{
-	case 1:strrev(string1);
-		printf("%s ", string1);
-		break;
-	case 2:strcpy(string2, string1);
-		printf("%s ", string2);
-		break;
-	case 3:printf("Enter the string2\n"); 
-		scanf_s("%s",string2,10);
-		strcat(string1, string2); printf("%s", string1);
-		break;
-	case 4:printf("Enter the second string for comparing:\n");
-		scanf_s("%s",string2,50);
-		result = strcmp(string1, string2);
-		if (result == 0)
-			printf("equal");
-		else if (result == 1)
-			printf("%s is greater than %s", string1, string2);
-		else if (result == -1)
-			printf("%s is less than %s", string1, string2);
-		break;
+		printf("\n\nEnter \n 0:EXIT \n 1.Reverse \n 2.Copy \n 3.Concatnate \n 4.Comparison : \n");
+		scanf_s("%d", &choice);
+		switch (choice)
+		{
+		case 1:printf("Enter string to be reversed :\n");
+			getchar();
+			scanf_s("%[^\n]s", string1, MAX);
+			str_reverse(string1, string2);
+			printf("Reversed string :%s ", string2);
+			break;
+		case 2:printf("Enter string to be copied :\n");
+			getchar();
+			scanf_s("%[^\n]s", string1, MAX);
+			str_copy(string1, string2);
+			printf("Copied string :%s", string2);
+			break;
+		case 3:printf("Enter first string :\n");
+			getchar();
+			scanf_s("%[^\n]s", string1, MAX);
+			printf("Enter second string :\n");
+			getchar();
+			scanf_s("%[^\n]s", string2, MAX);
+			str_cat(string1, string2);
+			printf("\n Concatnated string :%s", string1);
+			break;
+		case 4:printf("Enter first string :\n");
+			getchar();
+			scanf_s("%[^\n]s", string1, MAX);
+			printf("Enter second string :\n");
+			getchar();
+			scanf_s("%[^\n]s", string2, MAX);
+			str_compare(string1, string2);
+			break;
+		case 0:
+			free(string1);
+			free(string2);
+			getchar();
+			return 0;
+		default:
+			printf("\nINVALID CHOICE \n");
+		}
 	}
+
 	return 0;
 }
--- programs_instructed/expand_string/2.expand_string/Expand_string.cpp
+++ programs_instructed/expand_string/2.expand_string/Expand_string.cpp
@@ -1,15 +1,18 @@
 #include<string.h>
 #include<iostream>
 using namespace std;
+#define MAX 128
 void expand_string(char *s1, char *s2)
 {
 	int i = 0,j=0;
 	int c;
 	int l = strlen(s1);
 	if (s1[0] == '-'||s1[l-1]=='-')
-		throw "cannot have hyphen at front or back";
+		// if it is given -a- it will display the following error
+		throw "Hyphen cannot be at both front and back";
 	while ((c = s1[i++]) != '\0')
 	{
+		//if it is given a-a it will display just a
 		if (s1[i] == '-'&&s1[i + 1] == c)
 		{
 			s2[j++] = c;
@@ -17,9 +20,11 @@ void expand_string(char *s1, char *s2)
 
 		}
 		else if (s1[i] == '-'&&s1[i + 1] < c)
+			//if it is given f-a it will thrown an invalid expression as error
 			throw "invalid expression";
 		else if (s1[i] == '-'&&s1[i + 1] > c)
 		{
+			//if it is c-z it will print from c to z
 			i++;
 			while (c < s1[i])
 			{
@@ -29,6 +34,7 @@ void expand_string(char *s1, char *s2)
 			
 		else if (s1[i] == '-'&&s1[i + 1] == '-')
 		{
+			//if it is -- it will display the following error
 			throw "cannot have two hyphens continuously";
 		}
 		else
@@ -39,11 +45,11 @@ void expand_string(char *s1, char *s2)
 }
 int main()
 {
-	char *string1 = new char[20];
-	char *string2 = new char[40];
+	char *string1 = new char[MAX];
+	char *string2 = new char[MAX];
 	try
 	{
-		cout<<"Enter string 1:"<<endl;
+		cout<<"Enter string :"<<"\n";
 		cin>> string1;
 		expand_string(string1, string2);
 		cout<<"Expanded string is:"<<endl;
@@ -53,9 +59,10 @@ int main()
 	}
 	catch (const char*msg)
 	{
+		// the error thrown will be displayed her
 		cout << msg;
 	}
-	getchar();
-	getchar();
+	system("pause");
+
 	return 0;
 }
